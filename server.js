ee8ff434745be8a41a98748b97c6602c75217eea
Ubuntu <arj25022005@gmail.com>
2026-05-27 19:17:14 +0530

    errros fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import todoroute from './routes/routes.todo.js'
 import {connectDB} from './config/db.js'
+import { errorHandler } from './middlewares/error.middleware.js';
 
 const PORT = process.env.PORT||3002
 const app = express();
@@ -17,6 +18,9 @@ connectDB()
 
 // Routes
 app.use("/api/todos",todoroute)
+
+// error handler middleware
+app.use(errorHandler)
 
 //Server start point
 app.listen(PORT,(req,res)=>{
